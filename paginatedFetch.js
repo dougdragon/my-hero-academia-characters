420c9d46a51b162973e5dd068d150acedff5bc27
Doug Dragon <thatdougore@gmail.com>
2021-08-19 13:07:38 -0400

    initial commit

---
+++ paginatedFetch.js
@@ -0,0 +1,20 @@
+const paginatedFetch = (
+  url = 'https://myheroacademiaapi.com/api/character',
+  page = 1,
+  previousResponse = []
+) => {
+  return fetch(`${url}?page=${page}`)
+    .then(response => response.json())
+    .then(newResponse => {
+      const results = newResponse.result;
+      const response = [...previousResponse, ...results];
+
+      if (newResponse.info.currentPage !== newResponse.info.pages) {
+        page++;
+
+        return paginatedFetch(url, page, response);
+      }
+
+      return response;
+    });
+}
